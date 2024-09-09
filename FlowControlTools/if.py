287cba1e95d53c5141a6a0677287c3470db6fef5
Adarasha Gaihre <adarashagaihre80@nepathyacollege.edu.np>
2024-09-09 13:37:27 +0545

    adding 3 examples for flow control

---
+++ FlowControlTools/if.py
@@ -0,0 +1,39 @@
+x = int(input("Please enter an number: "))
+if x < 0:
+    print('Negative')
+elif x == 0:
+    print('Zero')
+elif x>0:
+    print('positive')
+
+
+y = input("Please enter an week: ")
+if y== 'sun':
+    print('Sunday')
+elif y== 'mon':
+        print('Monday')
+elif y== 'tue':
+        print('Tuesday')
+elif y== 'wed':
+        print('Wednesday')
+elif y== 'thu':
+        print('Thursday')
+elif y== 'fri':
+        print('Friday')
+elif y== 'sat':
+        print('Saturday')
+else:
+        print('Invalid input')
+
+
+
+
+
+z = int(input("Enter the time "))
+if z<24  and z < 12:
+       print("Morning CCN");
+elif z > 11 and z < 24:
+       print('Evening, CCN');
+else :
+       print('you seems to be a ghost 💀☠️')
+       
