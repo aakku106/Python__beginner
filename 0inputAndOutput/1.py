89ff1c7da2b6296014b852e0c6c1064f0102b38a
Adarasha Gaihre <adarashagaihre80@nepathyacollege.edu.np>
2024-09-13 23:10:56 +0545

    Input and output function with sep and +

---
+++ 0inputAndOutput/1.py
@@ -0,0 +1,25 @@
+# A conversation between two beings
+being1 = input("What's the name of 1st being ? ")
+being2 = input('"Enter the name of 2nd being"')
+print(being1,': Ohayyo ' , being2 , ' I am ' , being1)
+print(being2,':what the hek go away uou 🐒 ')
+
+# Another interesting example is making a tree
+print('   *')
+print('  ***')
+print(' *****')
+print('*******')
+print("  ||")
+
+# OR
+
+x = input('how shall the tre look')
+print('   ',x)
+print('  '+x,x,x)
+print(" "+x,x,x,x,x)
+print(x,x,x,x,x,x)
+print("    ||")
+
+# This above example takes any string and shape it like a tree 
+# In this example i have used , and + if they were same thing, cause typecasting is not needed here, everything is string here 
+# + concatenates and , just shop things one by one 
