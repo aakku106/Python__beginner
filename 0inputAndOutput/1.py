9b30da713320dfb927b17cb98651f143808f30b4
Adarasha Gaihre <adarashagaihre80@nepathyacollege.edu.np>
2024-09-17 23:16:09 +0545

    Add input and output function with sep and +
    Add examples for flow control using for loop and range
    
    Add examples for flow control with explanations
    
    Add examples for flow control
    
    Add initial commit

--- 0inputAndOutput/1.py
+++ 0inputAndOutput/1.py
@@ -23,3 +23,15 @@ print("    ||")
 # This above example takes any string and shape it like a tree 
 # In this example i have used , and + if they were same thing, cause typecasting is not needed here, everything is string here 
 # + concatenates and , just shop things one by one 
+
+age: int = 16;
+name = 'Aakku';
+height: float = 5.6;
+print('My name is',name,'and my age is',age,'and my height is',height)
+# This is how you can print multiple things in one line, you can use , to separate them, and it will automatically add a space between them
+# You can also use + to concatenate them, but you will have to typecast them to string, because + can't concatenate different types
+print(f'My name is {name} and my age is {age} and my height is {height}')
+# You can also use f string, which is the most efficient way to print multiple things in one line
+# You can use {} to add variables in the string, and it will automatically add them in the string same like $@ in C#
+
+
