e9279bcebd5dd4a4832897655942a597cde3428b
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2024-09-30 22:24:37 +0545

    Perform simple addition using hyphen and underscore

---
+++ 0.1Operations/py.py
@@ -0,0 +1,15 @@
+# perform simple addition <In wired way possible using hyphen[minus] and underscore>
+__=10;
+_=5;
+print(__--_);
+
+_-=__;
+print(_) 
+# int contains both positive and negative numbers in python
+if __>_:
+    print('the original value of 1st addition:' , __-_);
+# ☠️  
+
+
+    wee = 3;
+print(+-+-+-+-wee)
