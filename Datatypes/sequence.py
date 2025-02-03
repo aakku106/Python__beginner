e1a6914aec5114752e282b77175abc4fb242c6bb
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-02-04 00:17:56 +0545

    Add basic list operations and initial sorting implementation

--- Datatypes/sequence.py
+++ Datatypes/sequence.py
@@ -1,1 +1,9 @@
 # List,tuple and range
+
+
+
+# List:
+a=[1,2,3]
+a.append(0) #This is push operation
+a.pop(1);   #This is pop operation as name suggest
+print(a);
---
+++ dsa/Shorting/one.py
@@ -0,0 +1,8 @@
+array=[1,42,5,2,5,6,73];
+print('Given Array is:\t',array);
+for i in array:
+    minIndex=i;
+    for j in array:
+        minIndex=j;
+    temp=array.append(i);
+
